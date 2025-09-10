c4e10dda0ace0cdf64120e93956cc1c9dec4d0ea
queeniwu <queeniwu2017@gmail.com>
2025-09-10 14:17:16 -0400

    adding more content

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Wait for DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     const cursor = document.getElementById('cursor');
-    const competitorMattress = document.getElementById('mattress1');
-    const ourMattress = document.getElementById('mattress2');
+    const mattress1 = document.getElementById('mattress1');
+    const mattress2 = document.getElementById('mattress2');
     const mattress3 = document.getElementById('mattress3');
+    const mattress = document.getElementById('mattress');
 
 
  
@@ -20,22 +21,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Cursor state changes
     
-    if (competitorMattress) {
-        competitorMattress.addEventListener('mouseenter', function() {
+    if (mattress1) {
+        mattress1.addEventListener('mouseenter', function() {
             cursor.classList.add('sleep1');
         });
 
-        competitorMattress.addEventListener('mouseleave', function() {
+        mattress1.addEventListener('mouseleave', function() {
             cursor.classList.remove('sleep1');
         });
     }
 
-    if (ourMattress) {
-        ourMattress.addEventListener('mouseenter', function() {
+    if (mattress2) {
+        mattress2.addEventListener('mouseenter', function() {
             cursor.classList.add('sleep2');
         });
 
-        ourMattress.addEventListener('mouseleave', function() {
+        mattress2.addEventListener('mouseleave', function() {
             cursor.classList.remove('sleep2');
         });
     }
@@ -49,4 +50,23 @@ document.addEventListener('DOMContentLoaded', function() {
             cursor.classList.remove('sleep3');
         });
     }
+
+    if (mattress) {
+        mattress.addEventListener('mouseenter', function() {
+            cursor.classList.add('awake');
+        });
+
+        mattress.addEventListener('mouseleave', function() {
+            cursor.classList.remove('awake');
+        });
+    }
 });
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.menu');
+    if (window.scrollY > 50) {       // adjust threshold
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
